89d7852ba872614dad0e37aca564c356195d8a40
David Farago <farago@qpr-technologies.de>
2018-07-11 11:42:08 +0200

    add asio environment for udp processing

---
+++ cmake-build-debug/CMakeFiles/mia.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mia.dir/main.cpp.o"
+  "mia.pdb"
+  "mia"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
